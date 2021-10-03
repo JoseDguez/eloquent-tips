d268e8ac9abcf3ec68459e5e3e8357d9392f1bf8
Jose Antonio Dominguez <joseantonio.dguez@gmail.com>
2021-10-02 18:36:50 -0700

    Eloquent tip 01 app performance

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::query()
+            ->orderBy('name')
+            ->with('country')
+            ->simplePaginate();
+
+        return view('users', compact('users'));
+    }
+}
